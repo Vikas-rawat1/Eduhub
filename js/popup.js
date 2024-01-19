6c5868d3fbb6af565e7a4662a451beb254a8c572
vikas <rawatvikas689@gmail.com>
2024-01-20 03:42:26 +0530

    container config

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+
+function openPopup() {
+  document.getElementById("enrollPopup").style.display = "flex"
+}
+
+function closePopup() {
+  document.getElementById("enrollPopup").style.display = "none";
+}
+
+
