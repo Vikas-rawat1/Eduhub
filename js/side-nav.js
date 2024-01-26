e76b3d86f3ec3c9433797e81367bb146fc5b0c41
vikas <rawatvikas689@gmail.com>
2024-01-26 08:36:52 +0530

    container config

---
+++ js/side-nav.js
@@ -0,0 +1,9 @@
+function sideOpen() {
+  let open = document.getElementById("mobile-menu");
+  open.style.width = "90%";
+
+}
+function sideClose() {
+  let open = document.getElementById("mobile-menu");
+  open.style.width = "0";
+}
