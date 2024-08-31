71d806dfd61a4e14c3b1fa113b7f17725bb4958d
vikas <121391039+Vikas-rawat1@users.noreply.github.com>
2024-08-31 06:46:22 +0530

    Update move.js

--- js/move.js
+++ js/move.js
@@ -2,4 +2,4 @@ var pos = document.documentElement;
 pos.addEventListener('mousemove', e => {
   pos.style.setProperty('--x', e.clientX + 'px')
   pos.style.setProperty('--y', e.clientY + 'px')
-})  
+}) 
